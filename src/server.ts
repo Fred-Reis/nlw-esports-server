9de0ef644331f815e634fc2653c10fef8588a46e
frederico lopes <fredericolopes@fredericos-MacBook-Pro.local>
2022-09-15 22:17:09 -0300

    prisma added

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,19 @@ import express from "express";
 
 const app = express();
 
-app.get("/ads", (request, response) => {
+app.get("/games", (request, response) => {
+  return response.json([]);
+});
+
+app.post("/ads", (request, response) => {
+  return response.status(201).json([]);
+});
+
+app.get("/games/:id/ads", (request, response) => {
+  const gameId = request.params.id;
+
+  // return response.send(gameId);
+
   return response.json([
     { id: 1, ads: "Anuncio 1" },
     { id: 2, ads: "Anuncio 2" },
@@ -10,6 +22,14 @@ app.get("/ads", (request, response) => {
     { id: 4, ads: "Anuncio 4" },
     { id: 5, ads: "Anuncio 5" },
   ]);
+});
+
+app.get("/ads/:id/discord", (request, response) => {
+  const adId = request.params.id;
+
+  // return response.send(gameId);
+
+  return response.json([]);
 });
 
 app.listen(3333);
